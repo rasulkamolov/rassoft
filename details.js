43dcb74909ed054e33d08bd360559c1797dc5454
rasulkamolov <45510779+rasulkamolov@users.noreply.github.com>
2021-02-18 23:08:40 +0500

    Update details.js

--- details.js
+++ details.js
@@ -2,7 +2,7 @@ function moreInfo(){
 	
 	Swal.fire(
   'App DOs&DONTs?',
-  '1. Almost 99% of your link request downloadable. </br> 2. Redirects to download page <br> 3. With-Audio func is in development Shows only when wiut teachers used Display Share Mode, if Presentation Mode <br> is enabled, you will get only blank screen video or bad gateway <br>4. Number 3 is in development <br> Made with &hearts; by RK' ,
+  '1. Almost 99% of your link request downloadable. </br> 2. Redirects to download page <br> 3. With-Audio func is in development Shows only when wiut teachers used Display Share Mode, if Presentation Mode is enabled, you will get only blank screen video or bad gateway <br>4. Number 3 is in development <br> Made with &hearts; by RK' ,
   's'
 )
 }
